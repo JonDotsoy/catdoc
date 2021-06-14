560698a86ea07c1e0b13cb5130766699ff218586
Jonathan Delgado <hi@jon.soy>
2021-06-14 00:39:44 -0400

    feat: added definition openapi

--- packages/apidocsparser/src/toc-plus/Reference.ts
+++ packages/apidocsparser/src/toc-plus/Reference.ts
@@ -2,6 +2,7 @@ import path from "path"
 import util from "util"
 import { ItemReader } from "./ItemReader"
 import { Item } from "./Item"
+import { Openapi } from "../dto/openapi"
 
 export class Reference extends Item {
   readonly keyToc = this.itemReader.keyToc
@@ -10,9 +11,64 @@ export class Reference extends Item {
   readonly ["content-type"] = this.itemReader["content-type"]
   readonly bytes = this.itemReader.bytes
   readonly uri = this.itemReader.uri
+  openapiVersion?: string
+  info?: { title?: string; description?: string; version?: string }
+  paths?: {
+    title: string
+    description?: string
+    path: string
+    method: string
+  }[]
+  schemas?: {
+    title: string
+  }[]
+  body?: Openapi
 
   constructor(private itemReader: ItemReader) {
     super()
+    this.prepare()
+  }
+
+  prepare() {
+    const body =
+      typeof this.itemReader.body === "object" &&
+      !(this.itemReader.body instanceof Buffer)
+        ? (this.itemReader.body as Openapi)
+        : null
+    if (!body) return
+    this.body = body
+    this.openapiVersion = body.openapi
+    this.info = body.info
+    const paths: Reference["paths"] = []
+    const schemas: Reference["schemas"] = []
+
+    Object.entries<{ [method: string]: any }>(body.paths ?? {}).forEach(
+      ([path, methods]) => {
+        return Object.entries<{ summary?: string; description?: string }>(
+          methods
+        ).forEach(([method, obj]) => {
+          if (method !== "parameters") {
+            paths.push({
+              path,
+              method,
+              title: obj.summary ?? path,
+              description: obj.description,
+            })
+          }
+        })
+      }
+    )
+
+    Object.entries<{ title?: string }>(body.components?.schemas ?? {}).forEach(
+      ([schemaName, obj]) => {
+        schemas.push({
+          title: obj.title ?? schemaName,
+        })
+      }
+    )
+
+    this.paths = paths
+    this.schemas = schemas
   }
 
   [util.inspect.custom]: util.CustomInspectFunction = (depth, options) => {
@@ -22,6 +78,10 @@ export class Reference extends Item {
       "content-type": this["content-type"],
       charset: this.charset,
       bytes: this.bytes,
+      openapiVersion: this.openapiVersion,
+      info: this.info,
+      paths: this.paths,
+      schemas: this.schemas,
     })}`
   }
 }
